5b9f1af66373fc77b157988e086db21b609e4ba8
glsatz@gmail.com <glsatz@gmail.com@45ec40ab-35f4-8ce3-9c7d-be537e79909c>
2012-02-13 01:14:59 +0000

    Test harness updates.

--- trunk/test.sh
+++ trunk/test.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+if [ "$1" = "" ]; then
+  echo usage: test.sh host:port
+  exit 1
+fi
+host=http://$1
 for (( i = 1 ; i <= 10 ; i = i + 1 )) ; do
   echo loop $i
-  wget -r -l 1 -O /dev/null -o /dev/null 'http://backup:9900'
+  wget -r -l 1 -O /dev/null -o /dev/null $host
   sleep 1
-  wget -O /dev/null -o /dev/null --post-data="open=" 'http://backup:9900/devpost.html?dev=/dev/ttyUSB1&fn=open'
-  wget -O /dev/null -o /dev/null 'http://backup:9900'
+  wget -O /dev/null -o /dev/null --post-data="fn=open&dev=/dev/cu.SLAB_USBtoUART" $1'/devpost.html?dev=/dev/cu.SLAV_USBtoUART&fn=open'
+  wget -O /dev/null -o /dev/null $host
   j=20
   while [ $j -gt 0 ]; do
-    wget -O /tmp/poll.xml -o /dev/null 'http://backup:9900/poll.xml'
+    wget -O /tmp/poll.xml -o /dev/null $1'/poll.xml'
     if ( test -s /tmp/poll.xml && grep 'log size="0"' /tmp/poll.xml > /dev/null 2>&1 ) ; then
       let j--
     else
       j=100
     fi
   done
-  wget -O /dev/null -o /dev/null --post-data="close=" 'http://backup:9900/devpost.html?dev=/dev/ttyUSB1&fn=close'
-  wget -O /dev/null -o /dev/null 'http://backup:9900'
+  wget -O /dev/null -o /dev/null --post-data="fn=close&dev=/dev/cu.SLAB_USBtoUART" $1'/devpost.html?dev=/dev/cu.SLAB_USBtoUART&fn=close'
+  wget -O /dev/null -o /dev/null $host
 done
-wget -O /dev/null -o /dev/null --post-data="exit=" 'http://backup:9900/devpost.html?dev=&fn=exit'
+wget -O /dev/null -o /dev/null --post-data="fn=exit" $1'/devpost.html?dev=&fn=exit'
