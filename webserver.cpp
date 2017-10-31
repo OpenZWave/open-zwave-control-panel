8547542cbb9bcd1dd562769739e6fc7954f2de45
Thiago Oliveira <infratl@gmail.com>
2017-10-30 20:47:29 -0700

    fix typo on content-type header

--- webserver.cpp
+++ webserver.cpp
@@ -1138,7 +1138,7 @@ int Webserver::Handler (struct MHD_Connection *conn, const char *url,
 		else if (strcmp(url, "/devices.xml") == 0 && (devname != NULL || usb))
 			ret = SendDeviceListResponse(conn);
 		else if (strcmp(url, "/currdev") == 0) 
-			ret = web_send_data(conn, devname ? devname : "NULL", MHD_HTTP_OK, false, false, "text/pain"); // no free, no copy
+			ret = web_send_data(conn, devname ? devname : "NULL", MHD_HTTP_OK, false, false, "text/plain"); // no free, no copy
 		else
 			ret = web_send_data(conn, UNKNOWN, MHD_HTTP_NOT_FOUND, false, false, NULL); // no free, no copy
 		return ret;
